dbfcef54b90801892933dcc5ec28528b756ea7eb
Adithya S K <adithyaskolavi@gmail.com>
2023-03-18 13:57:53 +0530

    added pages folder - Adithya S K

---
+++ constants/index.js
@@ -0,0 +1,134 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: './assets/dataScience.png',
+    title: 'Data science',
+    tracks: [
+      'Multimedia security',
+      'Steganography',
+      'Digital watermarking',
+      'Biometric-based security systems',
+      'Face/feature recognition',
+      'AI/ML in signal processing for communications',
+      'WLAN',
+      'AI-based resource allocation',
+      'Optimization in wireless',
+      '6G communication',
+      'Energy efficient networking techniques',
+      'AI/ML for intelligent communications',
+    ],
+  },
+  {
+    id: 'world-2',
+    imgUrl: './assets/robotics.webp',
+    title: 'Robotics and Automation',
+    tracks: [
+      'Defence and Surveillance',
+      'Manufacturing sector',
+      'Automotive',
+      'Aerospace',
+      'Medical sector',
+      'Industrial IoT',
+      'Industry 4.0',
+    ],
+  },
+  {
+    id: 'world-3',
+    imgUrl: './assets/bigData.png',
+    title: 'Bigdata analytics',
+    tracks: [
+      'Artificial Intelligence',
+      'Deep Learning',
+      'Reinforcement learning',
+      'Cybersecurity and intelligence',
+      'Scientific research with massive data',
+      'Data Mining',
+      'Data Visualization',
+    ],
+  },
+  {
+    id: 'world-4',
+    imgUrl: './assets/appliedMath.png',
+    title: 'Applied Mathematics',
+    tracks: [
+      'Linear algebra',
+      'Differential equations and Fourier analysis',
+      'Differential and computational geometry',
+      'Probability and statistics',
+      'Numerical analysis',
+      'Operations research and optimization',
+      'Experimental physics',
+      'Chemical sciences',
+      'Biological science',
+    ],
+  },
+];
+
+export const startingFeatures = [
+  'Find a track that suits you',
+  'Create a team and pitch your idea',
+  'Get a mentor and start working on your project',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: './icons/test.svg',
+    title: 'Jackpot questions',
+    subtitle: 'A quiz with a jackpot prize for the winner. ',
+  },
+  {
+    imgUrl: './icons/online-learning.svg',
+    title: 'Mentorship',
+    subtitle: 'Get mentorship from officials from the industry',
+  },
+  {
+    imgUrl: './icons/online-learning.svg',
+    title: 'Mentorship',
+    subtitle: 'Get mentorship from officials from the industry',
+  },
+  {
+    imgUrl: './icons/online-learning.svg',
+    title: 'Mentorship',
+    subtitle: 'Get mentorship from officials from the industry',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: './assets/dataScience.png',
+    title: 'Data science',
+    subtitle:
+      'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: './assets/robotics.webp',
+    title: 'Robotics and Automation',
+    subtitle:
+      'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: './assets/bigData.png',
+    title: 'Bigdata analytics',
+    subtitle:
+      'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+  {
+    imgUrl: './assets/appliedMath.png',
+    title: 'Applied Mathematics',
+    subtitle:
+      'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/company/shunya-pes/mycompany/',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/shunya_pes/',
+  },
+];
