10d559c95613cefd608974a637264bf6eb997be1
Adithya S K <adithyaskolavi@gmail.com>
2023-03-18 14:00:36 +0530

    home page with components added - Adithya S K

---
+++ pages/index.jsx
@@ -0,0 +1,31 @@
+import { Navbar, Footer } from '../components';
+import { About, Explore, Feedback, Hero, WhatsNew, World } from '../sections';
+// import styles from '../styles';
+
+const Home = () => (
+  <div className="bg-primary-black overflow-hidden w-[100vw]">
+    <div className="relative">
+      <div className="gradient-03 z-0" />
+      <div className="gradient-04 z-0" />
+      <Navbar />
+      <Hero />
+    </div>
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+      <Feedback />
+    </div>
+    <World />
+    <div className="relative">
+      <div className="gradient-04 z-0" />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Home;
