b3163646c617d182d35fe4d1ebc74754938d34d3
Adithya S K <adithyaskolavi@gmail.com>
2023-03-18 14:00:51 +0530

    register page with components added - Adithya S K

---
+++ pages/register.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Navbar from '../components/Navbara';
+import Footer from '../components/Footera';
+import { GetStarted, Insights } from '../sections';
+// import styles from '../styles';
+
+const Home = () => (
+  <div className="bg-primary-black overflow-hidden w-[100vw]">
+    <div className="relative">
+      <div className="gradient-03 z-0" />
+      <div className="gradient-04 z-0" />
+      <Navbar />
+    </div>
+
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+    </div>
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Home;
