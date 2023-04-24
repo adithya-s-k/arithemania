496ce664b454f56e4f85e8880e426bc301b950f8
Adithya S K <adithyaskolavi@gmail.com>
2023-04-24 11:31:55 +0530

    updated env veriables - Adithya S K

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: 'arithmania-d394d.firebaseapp.com',
-  projectId: 'arithmania-d394d',
-  storageBucket: 'arithmania-d394d.appspot.com',
-  messagingSenderId: '526647876215',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: 'G-SQLKBRPBJS',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
